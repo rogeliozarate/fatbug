33ffcbc738d926ee67a718d2bed80a4211306cd4
Rogelio Zarate <cobizk@gmail.com>
2010-09-20 20:25:18 -0500

    fiel name format modified to reflect date,time, and size

--- fatbug.rb
+++ fatbug.rb
@@ -6,8 +6,8 @@ fatbug.rb
       eco-systems
       
   Creates and saves files of the specified size
-  Useful
-   to create dummy load files
+  Useful to create dummy load files
+  ? add UUID
 =end
 
 thesize = 0.200
@@ -17,7 +17,7 @@ print 'You ordered a ' + thesize.to_s + ' MB file.'
 string = "abcdefghijklmnopqrstuvwxyz1234567890"
 filesize = 0;
 
-f = File.new(Time.now.strftime("%Y%m%d-%H%M%S") + ".dummy", 'w')
+f = File.new("#{Time.now.strftime("%Y%m%d-%H%M%S")}-#{thesize}MB.dummy", 'w')
 
 while filesize < thesize * 1e6
   f.puts string
